7fe5f5f2127788ad209f48eea090b6f2a482da56
Yerra Srija <112320203+Srija2003@users.noreply.github.com>
2025-02-03 19:36:01 +0530

    Delete hello_foo.py

--- hello_foo.py
+++
@@ -1,4 +0,0 @@
-first_name = input("what is your name?")
-last_name = input("what is your last name?")
-address = input("enter your address")
-print(f"hello{first_name} {last_name}, your address is {address}")
